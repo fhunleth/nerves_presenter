c5f0c2c289918885f30763e620435f560e06152b
Frank Hunleth <fhunleth@troodon-software.com>
2016-08-13 20:26:20 -0400

    Bump br and the archive

--- apps/fw/mix.exs
+++ apps/fw/mix.exs
@@ -7,7 +7,7 @@ defmodule Fw.Mixfile do
     [app: :fw,
      version: "0.0.1",
      target: @target,
-     archives: [nerves_bootstrap: "0.1.3"],
+     archives: [nerves_bootstrap: "~> 0.1.3"],
      deps_path: "deps/#{@target}",
      build_path: "_build/#{@target}",
      build_embedded: Mix.env == :prod,
